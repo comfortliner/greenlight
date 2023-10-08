4d865456aa1e1a1fdce4afb4dd4c849fceddec63
comfortliner <andreas_werz@gmx.de>
2023-10-08 16:40:15 +0200

    Make use if the new slices.Contains() function in the PermittedValues() validation helper.

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -2,6 +2,7 @@ package validator
 
 import (
 	"regexp"
+	"slices"
 )
 
 var (
@@ -39,12 +40,7 @@ func (v *Validator) Check(ok bool, key, message string) {
 
 // Generic function which returns true if a specific value is in a list.
 func PermittedValue[T comparable](value T, permittedValues ...T) bool {
-	for i := range permittedValues {
-		if value == permittedValues[i] {
-			return true
-		}
-	}
-	return false
+	return slices.Contains(permittedValues, value)
 }
 
 // Matches returns true if a string value matches a specific regexp pattern.
